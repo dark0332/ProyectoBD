321e33bff85bde5a9895837712622e59191df3b8
dark0332 <manuel.bravo.m8@gmail.com>
2019-12-15 17:36:53 -0600

    v1.0
    Menu Principal

--- src/proyectobd/ProyectoBD.java
+++ src/proyectobd/ProyectoBD.java
@@ -15,7 +15,8 @@ public class ProyectoBD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Principal nx = new Principal();
+        nx.setVisible(true);
     }
-    
+
 }
